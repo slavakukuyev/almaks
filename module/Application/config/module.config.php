e8ee9f5187a37dc94c8c4b65b92d95f9a559c3a2
Slava <slava.kukuyev@gmail.com>
2014-04-27 18:50:06 +0300

    Contact controller added

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -73,7 +73,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'Application\Controller\Index' => 'Application\Controller\IndexController',
+            'Application\Controller\Contact' => 'Application\Controller\ContactController'
         ),
     ),
     'view_manager' => array(
