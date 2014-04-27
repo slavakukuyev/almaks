4c6c08bebca151434a506089c16ec9a06ab56f22
Slava <slava.kukuyev@gmail.com>
2014-04-27 18:17:25 +0300

    create home page

--- module/Almaks/config/module.config.php
+++ module/Almaks/config/module.config.php
@@ -1,3 +1,28 @@
 <?php
+
 return array(
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view'
+        )
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Almaks\Controller\Index' => 'Almaks\Controller\IndexController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'sayhello' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route' => '/almaks',
+                    'defaults' => array(
+                        'controller' => 'Almaks\Controller\Index',
+                        'action' => 'index',
+                    )
+                )
+            )
+        )
+    ),
 );
