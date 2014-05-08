418fc7f475cee1e7ccace3ec4d7a831d1de11714
Slava <slava.kukuyev@gmail.com>
2014-05-08 23:13:11 +0300

    Start contact form

--- module/Application/src/Application/Controller/ContactUsController.php
+++ module/Application/src/Application/Controller/ContactUsController.php
@@ -4,13 +4,34 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Application\Form\ContactForm; 
+use Application\Form\ContactValidator; 
 
 class ContactUsController extends AbstractActionController
 {
     public function indexAction()
     {
+        $form = new ContactForm(); 
+    $request = $this->getRequest(); 
+
+    if($request->isPost()) 
+    { 
+        $user = new (); 
         
-        return new ViewModel();
+        $formValidator = new ContactValidator(); 
+        { 
+            $form->setInputFilter($formValidator->getInputFilter()); 
+            $form->setData($request->getPost()); 
+        } 
+         
+        if($form->isValid()){ 
+        { 
+            $user->exchangeArray($form->getData()); 
+        } 
+    } 
+    
+    
+        return new ViewModel(array('form' => $form));
     }
 }
 
